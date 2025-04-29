3a9e967ad11cfa8833513b4f02a4b87589ee481f
Miranda Mundt <mmundt@sandia.gov>
2025-04-29 13:08:24 -0600

    Change tol

--- pyomo/solvers/tests/checks/test_xpress_persistent.py
+++ pyomo/solvers/tests/checks/test_xpress_persistent.py
@@ -65,7 +65,7 @@ class TestXpressPersistent(unittest.TestCase):
         self.assertAlmostEqual(m.x.value, -0.4, delta=1e-6)
         self.assertAlmostEqual(m.y.value, 0.2, delta=1e-6)
         opt.load_vars()
-        self.assertAlmostEqual(m.x.value, 0, delta=1e-6)
+        self.assertAlmostEqual(m.x.value, 0, delta=3e-6)
         self.assertAlmostEqual(m.y.value, 1, delta=2e-6)
 
         opt.remove_constraint(m.c2)
